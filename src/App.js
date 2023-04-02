2352e5ea80627267f4c3565d76377778ccb8c3c2
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-04-02 16:39:09 -0500

    mejora del componente courseItem inicial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 
 function App(){
     return(
@@ -10,36 +10,13 @@ function App(){
 
 export default App;
 
-const listacuros={
-    materias:[{
-        nombre:"español",
-        ID:445,
-        decripcion:"materia español para aprender español"
-    },
-{   nombre:"cálculo",
-    id: Math.floor(Math.random()* 10000),
-    decripcion:"materias esenciales para desarrollo de pensamiento lógico matemático "
-},
-{
-    nombre:"Lógica Digital",
-    id: Math.floor(Math.random()* 10000),
-    decripcion:"materia desarrollo lógico en circuitos"
+ function CourseList(){
+    const [curso, setcurso] = useState([
+        {id: Math.floor(Math.random), nombre: 'calculo', descripcion: 'para aprender matematicas'},
+        {id: Math.floor(Math.random), nombre: 'Ingles', descripcion: 'para aprender idiomas'},
+        {id: Math.floor(Math.random), nombre: 'fisica', descripcion: 'para aprender leyes de newton'}
+    ]);
 
-},
-{
-    nombre:"Física",
-    id: Math.floor(Math.random()* 10000),
-    decripcion:"materia para comprender el cómo funciona nuestro universo"
-},
-{
-    nombre:"PNT",
-    id: Math.floor(Math.random()* 10000),
-    decripcion:"Desarrollo del mundo de java script, css, html y mucho mas"
-}
-
-]   
-}   
-
-const CurseItem = () =><div> </div>;
     
+ };
 
