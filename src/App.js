df1cca38b2cf3c283f26a7330f7048da1edfc710
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-04-01 19:26:27 -0500

    creacion de el objeto listacurso, con sus materias

--- src/App.js
+++ src/App.js
@@ -7,3 +7,28 @@ function App(){
 }
 
 export default App;
+
+const listacuros={
+    materias:[{
+        nombre:"español",
+        ID:445,
+        decripcion:"materia español para aprender español"
+    },
+{   nombre:"cálculo",
+    ID:446,
+    decripcion:"materias esenciales para desarrollo de pensamiento lógico matemático "
+},
+{
+    nombre:"Lógica Digital",
+    ID:447,
+    decripcion:"materia desarrollo lógico en circuitos"
+
+},
+{
+    nombre:"Física",
+    ID:448,
+    decripcion:"materia para comprender el cómo funciona nuestro universo"
+}
+
+]   
+}       
