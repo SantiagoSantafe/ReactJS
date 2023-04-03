66854bb849907ecc6424fc2ea0e9b6403a603bee
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-04-02 19:53:26 -0500

    Renderiza la lista de los cursos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import { Component, useState } from "react";
-
-
-export default App;
-
- function CourseList(){
-    const [curso, setcurso] = useState([
+import React from 'react';
+import { useState } from 'react';
+export const CourseList=()=>{
+    const [curso, setcurso] = useState(
+        [
         {id: Math.floor(Math.random), 
             nombre: 'calculo', 
             descripcion: 'para aprender matematicas'
         },
         {id: Math.floor(Math.random),
-             nombre: 'Ingles', 
-             descripcion: 'para aprender idiomas'},
+        nombre: 'Ingles', 
+        descripcion: 'para aprender idiomas'},
 
         {id: Math.floor(Math.random), 
             nombre: 'fisica', 
             descripcion: 'para aprender leyes de newton'
-        }
-    ]);
+        },
+    ])
 
     const CourseItem= (Textname, textdescrip) =>{
         const nuevocurso = {id: Math.floor(Math.random)(), nombre: Textname, descripcion: textdescrip};
@@ -28,11 +26,22 @@ export default App;
         const nuevocurso = curso.filter((curso)=> curso.id );
         setcurso(nuevocurso);
     };
+    const CourseItem1=curso.map((materia)=>{
+        
+        return(
+            <li>
+                <p key={materia.id.toString}>{materia.id}</p>
+                <p key={materia.nombre.toString}>{materia.nombre}</p>
+                <p key={materia.descripcion.toString}>{materia.descripcion}</p>
+            </li>
+            
+        )                
+    })
+        return (
+            <>
+            {CourseItem("porno","acto sexual")}
+            <ul>{CourseItem1}</ul>
 
-
-    return (
-        <><h1> PEPEPIPIOLO</h1> 
-        </>
-    )
- };
-
+            </>
+        )
+    };
